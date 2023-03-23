445810f9c6cf0188de067cc8aa3b11f6c46c344e
Sora952 <sora952c16b10@gmail.com>
2023-03-23 13:35:09 +0100

    modif

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" class="img" src="./assets/mouton.jpeg" />
+  <div id="t">
+    <VueWriter
+      :array="arr"
+      :typeSpeed="70"
+      :iterations="'1'"
+      :eraseSpeed="50"
+      :delay="1000"
+    />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return { arr: ["Hello !", "Cyrielle c'est la meilleure.", "Bêêêêêh !"] };
+  },
+};
 </script>
 
 <style>
+.typewriter.content *:last-child::after {
+  font-size: calc(3em + 20px) !important;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +30,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#t {
+  width: 120px;
+  height: 40px;
+  background-color: rgba(240, 240, 240, 0.5);
+  position: absolute;
+  top: 275px;
+  left: 650px;
+  border: solid 1px black;
+  border-radius: 8px;
+  padding: 15px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import VueWriter from "vue-writer";
 
-createApp(App).mount('#app')
+createApp(App).use(VueWriter).mount("#app");
