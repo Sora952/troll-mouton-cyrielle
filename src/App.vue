128ea9d4aa68d9d7a74110fc4d4f8eb89e320c9b
Sora952 <59966470+Sora952@users.noreply.github.com>
2023-03-24 13:38:01 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-  <img alt="Vue logo" class="img" src="./assets/mouton.jpeg" />
   <div id="texte">
     <VueWriter
       :array="arr"
@@ -30,10 +29,13 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-img {
+  
   width: 100%;
+  height: 100vh;
+  background-image: url("/img/mouton.3be4e771.jpeg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: 36% 36%;
 }
 
 #texte {
@@ -41,8 +43,8 @@ img {
   height: 40px;
   background-color: rgba(240, 240, 240, 0.5);
   position: absolute;
-  top: 275px;
-  left: 650px;
+  top: 250px;
+  left: 685px;
   border: solid 1px black;
   border-radius: 8px;
   padding: 15px;
